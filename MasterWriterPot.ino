6c87b9081a9bf771c480a15ad185b720540c35a8
Dipti Kulkarni <cyber.drk@gmail.com>
2017-10-01 18:32:41 +0530

    Master Writer via I2C for one potentiometer
    Use this to check if the I2C connections from the master board is working right.

---
+++ MasterWriterPot.ino
@@ -0,0 +1,34 @@
+//Master Writer Potentiometer 
+//Transmits the mapped value of a potentiometer via I2C to a slave that is connected to a servo
+//Edits made by Dipti Kulkarni <<https://github.com/cyberdrk>>
+//Acknowledgements and grateful to the official Arduino Wire Master Writer tutorial  by Nicholas Zambetti <http://www.zambetti.com>
+
+
+
+
+#include <Wire.h>
+
+int potpinblue = 0;  // analog pin used to connect the potentiometer
+int valblue;    // variable to read the value from the analog pin
+
+void setup() 
+{
+  Wire.begin(); // join i2c bus (address optional for master)
+  Serial.begin(9600);
+}
+
+byte x = 0;
+
+void loop() 
+{
+  Wire.beginTransmission(8); // transmit to device #8
+  valblue = analogRead(potpinblue);            // reads the value of the potentiometer (value between 0 and 1023)
+  Serial.println(valblue);
+  valblue = map(valblue, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+
+  Wire.write("valblue is ");        // sends five bytes
+  Wire.write(valblue);              // sends one byte
+  Wire.endTransmission();    // stop transmitting
+
+  delay(500);
+}
